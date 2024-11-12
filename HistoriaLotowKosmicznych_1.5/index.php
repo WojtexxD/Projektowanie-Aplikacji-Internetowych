5260f85ce7ea7e047c120b9188186181d3d912d0
Wojciech <115220226+WojtexxD@users.noreply.github.com>
2024-11-12 21:51:33 +0100

    Add files via upload

--- HistoriaLotowKosmicznych_1.5/index.php
+++ HistoriaLotowKosmicznych_1.5/index.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 
+<html lang="pl">
+
 <?php
 	error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 ?>
-<html lang="pl">
 
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="css/Kontakt.css">
 		<link rel="stylesheet" href="css/HistoriaLotowKosmicznych.css">
+		<link rel="stylesheet" href="css/Wyslani w kosmos.css">
+		<link rel="stylesheet" href="css/Statki kosmiczne.css">
+		<link rel="stylesheet" href="css/Kontakt.css">
+		<link rel="stylesheet" href="css/Planety_Karlowate.css">
+		<link rel="stylesheet" href="css/Ksiezyce_Planet.css">
+		<link rel="stylesheet" href="css/Historia.css">
+		<link rel="stylesheet" href="css/Filmy.css">
 		<link rel="shortcut icon" href="Image/Logo.jpg">
 		<script src="js/timedate.js" type="text/javascript"> </script>
 	</head>
@@ -17,36 +24,47 @@
 	<body onload="startclock()">
 		<nav>
 			<ul>
-				<li><a href="Subpages/Historia.html"> Historia </a></li>
+				<li><a href="index.php"> Strona główna </a></li>
+			</ul>
+			<ul>
+				<li><a href="?idp=Historia"> Historia </a></li>
+				<li><a href="?idp=Filmy"> Filmy </a></li>
 			</ul>
 			<ul>
 				<li><a href=""> Ciała niebieskie </a>
 					<ol>
-						<li><a href="Subpages/Planety.html"> Planety </a></li>
-						<li><a href="Subpages/Planety_Karlowate.html"> Planety karłowate </a></li>
-						<li><a href="Subpages/Ksiezyce_Planet.html"> Księżyce planet </a></li>
+						<li><a href="?idp=Planety"> Planety </a></li>
+						<li><a href="?idp=Planety_Karlowate"> Planety karłowate </a></li>
+						<li><a href="?idp=Ksiezyce_Planet"> Księżyce planet </a></li>
 					</ol>
 				</li>
 			</ul>
 			<ul>
 				<li><a href=""> Galeria </a>
 					<ol>
-						<li><a href="Subpages/Statki kosmiczne.html"> Statki kosmiczne </a></li>
-						<li><a href="Subpages/Wyslani w kosmos.html"> Wysłani w kosmos (zwierzęta/ludzie) </a></li>
+						<li><a href="?idp=Statki_kosmiczne"> Statki kosmiczne </a></li>
+						<li><a href="?idp=Wyslani_w_kosmos"> Wysłani w kosmos (zwierzęta/ludzie) </a></li>
 					</ol>
 				</li>
 			</ul>
 			<ul>
-				<li><a href="Subpages/Kontakt.html"> Kontakt </a></li>
-			</ul>	
+				<li><a href="?idp=Kontakt"> Kontakt </a></li>
+			</ul>
 		</nav>
 		
 		<?php
-			if($_GET['idp']=='Kontakt')$strona='Subpages/Kontakt.html';
 			if($_GET['idp']=='')
 			{
 				$strona='Subpages/glowna.html';
 			}
+			if($_GET['idp']=='Kontakt')$strona='Subpages/Kontakt.html';
+			if($_GET['idp']=='Statki_kosmiczne')$strona='Subpages/Statki kosmiczne.html';
+			if($_GET['idp']=='Wyslani_w_kosmos')$strona='Subpages/Wyslani w kosmos.html';
+			if($_GET['idp']=='Planety')$strona='Subpages/Planety.html';
+			if($_GET['idp']=='Planety_Karlowate')$strona='Subpages/Planety_Karlowate.html';
+			if($_GET['idp']=='Ksiezyce_Planet')$strona='Subpages/Ksiezyce_Planet.html';
+			if($_GET['idp']=='Historia')$strona='Subpages/Historia.html';
+			if($_GET['idp']=='Filmy')$strona='Subpages/Filmy.html'; 
 			
 			include($strona);
 			
